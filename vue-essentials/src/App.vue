a124c6154ad3d4024888cc4e4d8d9bf5ea636b77
michel <msampil@switch.com.uy>
2023-08-15 18:23:34 -0300

    more componets

--- vue-essentials/src/App.vue
+++ vue-essentials/src/App.vue
@@ -5,6 +5,8 @@ import TodoList from './components/TodoList.vue';
 import FilteredTodoList from './components/FilteredTodoList.vue';
 import FilteredTodosNoComputed from './components/FilteredTodosNoComputed.vue';
 import DayNightSwitch from './components/DayNightSwitch.vue';
+import Todo from './components/Todo.vue';
+import List from './components/List.vue';
 </script>
 
 <template>
@@ -30,16 +32,39 @@ import DayNightSwitch from './components/DayNightSwitch.vue';
     <div class="container">
       <FilteredTodoList />
     </div>
-
+    <div class="fit-wrapper-group">
+      <div class="container fit-wrapper">
+        <Todo />
+      </div>
+      <div class="container fit-wrapper">
+        <Todo />
+      </div>
+      <div class="container fit-wrapper">
+        <Todo />
+      </div>
+    </div>
+    <div class="container">
+      <List />
+    </div>
   </main>
 </template>
 
 <style>
+.fit-wrapper-group {
+  display: flex;
+}
+
 .container {
   padding: 10px;
   margin: 20px 0 20px 0;
   box-shadow: 0 0 10px #02AAB0;
   border-radius: 5px;
+}
+
+.fit-wrapper {
+  width: fit-content;
+  padding: 20px;
+  margin: 0px 5px 0px 5px;
 }
 </style>
 
---
+++ vue-essentials/src/components/List.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+import ListItem from './ListItem.vue'
+
+const newCardTitles = ref(['I like 🥑', 'I love 🥔', `I'm from 🪐`]);
+</script>
+
+<template>
+  <h1> Happy List 😛</h1>
+  <ListItem v-for="cardTitle in newCardTitles" :key="cardTitle" :title="cardTitle" />
+</template>
